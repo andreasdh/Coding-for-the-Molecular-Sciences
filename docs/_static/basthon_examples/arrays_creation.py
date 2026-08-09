039f4577832c83fef94befef31efc33eaedabcfd
Andreas Haraldsrud <a_haraldsrud@hotmail.com>
2026-08-09 14:54:09 +0200

    Add English array creation example

---
+++ docs/_static/basthon_examples/arrays_creation.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from_list = np.array([1, 2, 3, 4])
+with_arange = np.arange(0, 11, 2)
+with_linspace = np.linspace(0, 10, 6)
+zeros = np.zeros(5)
+ones = np.ones(5)
+
+print("From a list:", from_list)
+print("arange:", with_arange)
+print("linspace:", with_linspace)
+print("zeros:", zeros)
+print("ones:", ones)

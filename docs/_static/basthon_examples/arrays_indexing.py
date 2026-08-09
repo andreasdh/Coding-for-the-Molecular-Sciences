fd944abc52d38f0a89566653412015ed2377f53a
Andreas Haraldsrud <a_haraldsrud@hotmail.com>
2026-08-09 14:54:17 +0200

    Add English array indexing example

---
+++ docs/_static/basthon_examples/arrays_indexing.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+values = np.array([0.12, 0.25, 0.37, 0.49, 0.61])
+
+print("First element:", values[0])
+print("Element at index 2:", values[2])
+print("Elements from index 1 to 3:", values[1:4])
+print("Final element:", values[-1])
